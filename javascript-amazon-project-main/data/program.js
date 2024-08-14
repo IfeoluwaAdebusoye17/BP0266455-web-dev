4863fb577a5727c529d0d9b125ba41c2edb6288a
Ife Ade <padepoju44@gmail.com>
2024-08-15 00:58:12 +0100

    genetate HTML for course summary

--- javascript-amazon-project-main/data/program.js
+++ javascript-amazon-project-main/data/program.js
@@ -1,7 +1,16 @@
-export let program = [];
+export let program = [{
+  courseId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+}, {
+  courseId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}];
 
 export function selectProgram(courseId) {
-  program[0] = courseId;
+  program[0] = {
+    courseId: courseId,
+    quantity: 1
+  };
   console.log(program);
 }
 

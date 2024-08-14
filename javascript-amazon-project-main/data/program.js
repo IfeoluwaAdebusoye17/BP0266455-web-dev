d7f59ff2f2cbd3866177766c0ebb0a73cf5a84b1
Ife Ade <padepoju44@gmail.com>
2024-08-14 23:12:22 +0100

    View course update

---
+++ javascript-amazon-project-main/data/program.js
@@ -0,0 +1,4 @@
+let program = '';
+
+
+console.log(program);
--- javascript-amazon-project-main/scripts/lloyds.js
+++ javascript-amazon-project-main/scripts/lloyds.js
@@ -46,13 +46,31 @@ products.forEach((product) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-course-id="${product.id}">
         Add to Cart
       </button>
     </div>
   `;
 });
 
-console.log(productsHTML);
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const courseId = button.dataset.courseId
+
+      //console.log(courseName);
+      /*
+      program.push({
+        courseName: courseName,
+        quantity: 1
+      });
+      */
+      program = courseId;
+      console.log(program);
+
+
+    });
+  });

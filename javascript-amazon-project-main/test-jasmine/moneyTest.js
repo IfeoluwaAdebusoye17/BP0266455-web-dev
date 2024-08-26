414adb0ae24bfe0594bbc3bc2fbe62c95d63ca23
Ife Ade <padepoju44@gmail.com>
2024-08-26 10:06:36 +0100

    included new jasmine

--- javascript-amazon-project-main/test-jasmine/moneyTest.js
+++
@@ -1,7 +0,0 @@
-import {formatCurrency} from '../scripts/utils/money.js'
-
-describe('test suite: formatCurrency', () => {
-  it('converts cents to dollars', () => {
-    expect(formatCurrency(2095)).toEqual('20.95');
-  })
-});

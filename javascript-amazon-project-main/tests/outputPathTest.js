fc5bc9c0396f7c710b6404fb804803c29f474d8a
Ife Ade <padepoju44@gmail.com>
2024-08-26 18:30:25 +0100

    wording chage

--- javascript-amazon-project-main/tests/outputPathTest.js
+++ javascript-amazon-project-main/tests/outputPathTest.js
@@ -3,7 +3,7 @@ import { filterRelevant } from "../scripts/lloyds.js";
 
 
 describe('test suite: output for training path', () => {
-    it('proves that the function is there which filters through selectable drop down', () => {
+    it('proves that the filter feature is there', () => {
         expect(filterRelevant()).not.toEqual('');
     });
 });

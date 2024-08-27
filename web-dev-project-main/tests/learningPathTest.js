69a164bfe64e2db86afe0a8ea408332cd9798d03
Ife Ade <padepoju44@gmail.com>
2024-08-27 15:43:47 +0100

    Everything is done Prasie God

--- web-dev-project-main/tests/learningPathTest.js
+++ web-dev-project-main/tests/learningPathTest.js
@@ -1,11 +1,11 @@
 import { selectPath, currentSelectedPath, loadFromStorage } from "../data/selected-path-status.js";
 
-/*Mocking to remove all items from program landing page*/
+/*Mocking to remove all items from learning path landing page*/
 /*We have also mocked set so our tests do not effect the local storage*/
 
 
-describe('test suite: selectedProgram', () => {
-    it('takes user to selected program', () => {
+describe('test suite: selectedLearningPath', () => {
+    it('takes user to selected learningPath', () => {
         spyOn(localStorage, 'setItem');
         
         spyOn(localStorage, 'getItem').and.callFake(() => {
@@ -21,7 +21,7 @@ describe('test suite: selectedProgram', () => {
 
     });
 
-    it('replaces already selected program', () => {
+    it('replaces already selected learning path', () => {
         spyOn(localStorage, 'setItem');
 
         selectPath('15b6fc6f-327a-4ec4-896f-486349e85a3d');
